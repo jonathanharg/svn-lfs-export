9c23a54dfdcf58b72c97f2866995e6e1fc59ed92
Jonathan Hargreaves <jonathanharg@outlook.com>
2026-02-01 22:25:00 +0000

    Log info to stderr
    Bypass fast-import entirely

--- src/utils.hpp
+++ src/utils.hpp
@@ -12,5 +12,5 @@ inline void LogError(fmt::format_string<T...> fmt, T&&... args)
 template <typename... T>
 inline void LogInfo(fmt::format_string<T...> fmt, T&&... args)
 {
-	fmt::println("progress {}", fmt::format(fmt, std::forward<T>(args)...));
+	fmt::println(std::cerr, fmt, std::forward<T>(args)...);
 }
